0d19153bb503f9af6cb3fc652568ca4ecac8f2a7
gabe thomas <gabe.thomas@bizzabo.com>
2021-06-07 22:31:01 -0400

    deploying

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Fitness-Tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/polar-chamber-22048", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
