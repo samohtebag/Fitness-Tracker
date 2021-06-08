1910a936359e921b2f476823b2c71880b648ce01
gabe thomas <gabe.thomas@bizzabo.com>
2021-06-07 21:08:55 -0400

    wrote server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ 
+    extended:true 
+}));
+
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Fitness-Tracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`Now running on this port: ${PORT}!`);
+});
