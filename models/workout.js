42783b25cd8abc8aef2a7be84129cd94b918bd65
gabe thomas <gabe.thomas@bizzabo.com>
2021-06-07 21:01:33 -0400

    finished models

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const schema = mongoose.schema;
+
+const workoutSchema = new schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+
+module.exports = workout;
