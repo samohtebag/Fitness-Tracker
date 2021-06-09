152f708a4b55313a169a5e5c6d4295e164dc2a67
gabe thomas <gabe.thomas@bizzabo.com>
2021-06-08 20:12:01 -0400

    redid apiroute

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,6 +1,8 @@
 const db = require("../models");
 
 module.exports = function(app) {
+
+  // GET request
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({}).then(dbWorkout => {
             res.json(dbWorkout);
@@ -9,13 +11,18 @@ module.exports = function(app) {
             res.status(400).json(err);
         });
     })
+
+  // GET request
     app.get("/api/workouts/range", ({}, res) => {
+
       db.Workout.find({}).then((dbWorkout) => {
         res.json(dbWorkout);
       }).catch(err => {
         res.status(400).json(err);
       });
     });
+
+    // POST request
     app.post("/api/workouts/", (req, res) => {
         db.Workout.create(req.body).then((dbWorkout) => {
           res.json(dbWorkout);
@@ -23,6 +30,8 @@ module.exports = function(app) {
             res.status(400).json(err);
           });
       });
+
+      // PUT request
       app.put("/api/workouts/:id", (req, res) => {
         db.Workout.findByIdAndUpdate(
           { _id: req.params.id }, { exercises: req.body }
