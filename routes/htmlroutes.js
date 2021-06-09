a85bef7f389582763a4bd9f1eb3b0466b57896e1
gabe thomas <gabe.thomas@bizzabo.com>
2021-06-08 20:06:59 -0400

    rewrote API route

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 module.exports = function(app) {
 
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    })
 
     app.get("/exercise", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
